59d7ce43e7150a3fa70e144aefcf7311cd27c3c5
adixshrma1 <aditya.sharma.26092001@gmail.com>
2024-09-13 15:13:11 +0530

    age logic corrected in Project 6

--- 06ageCalculator/script.js
+++ 06ageCalculator/script.js
@@ -5,13 +5,40 @@ let result = document.getElementById('result');
 function calculateAge(){
     // my logic
     let birthDate = new Date(inputBox.value);
-    let currentDate = new Date();
-    let milliSec = currentDate - birthDate;
-    let yearValue = milliSec/(1000*60*60*24*365)
 
-    let years = Math.floor(yearValue)
-    let months = Math.floor((yearValue - years)*12);
-    let days = Math.floor(((yearValue - years)*12 - months)*30);
+    let d1 = birthDate.getDate();
+    let m1 = birthDate.getMonth() +1;
+    let y1 = birthDate.getFullYear();
+
+    let currentDate = new Date();
+
+    let d2 = currentDate.getDate();
+    let m2 = currentDate.getMonth() +1;
+    let y2 = currentDate.getFullYear();
+
+    let days, months, years;
+    years = y2 - y1;
+
+    if(m2 >= m1) months = m2 - m1;
+    else{
+        years--;
+        months = 12 + m2 - m1;
+    }
+
+    if(d2 >= d1) days = d2 - d1;
+    else{
+        months--;
+        days = getDaysInMonth() + d2 - d1;
+    }
+
+    if(months < 0){
+        years--;
+        months = 11;
+    }
+
+    function getDaysInMonth(){
+        return new Date(years, months, 0).getDate();
+    }
 
     result.innerHTML = `You are <span>${years}</span> years, <span>${months}</span> months and <span>${days}</span> days old.`
 }
