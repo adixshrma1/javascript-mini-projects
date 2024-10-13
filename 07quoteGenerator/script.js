b44941c209f8522733124b5d40305a96b6befadf
adixshrma1 <aditya.codez26@gmail.com>
2024-10-13 18:12:56 +0530

    API key changed

--- 07quoteGenerator/script.js
+++ 07quoteGenerator/script.js
@@ -1,16 +1,32 @@
-let quotes = document.getElementById('quotes');
-let writer = document.getElementById('writer');
-let url = 'https://api.quotable.io/random';
+let quotes = document.getElementById("quotes");
+let writer = document.getElementById("writer");
 
-async function getQuote(){
-    const response = await fetch(url);
-    const data = await response.json();
+const apiKey = "ldRInyV03+ccAp+MJPJmCQ==uZK1hVuiGAsKaa8D";
+const url = "https://api.api-ninjas.com/v1/quotes";
 
-    quotes.innerHTML = data.content;
-    writer.innerHTML = data.author;
+async function getQuote() {
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      "X-Api-Key": apiKey,
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await response.json();
+  console.log(data)
+
+  quotes.innerHTML = data[0].quote;
+  writer.innerHTML = data[0].author;
 }
 getQuote();
 
-function tweetIt(){
-    window.open("https://twitter.com/intent/tweet?text=" + quotes.innerHTML + "---by " + writer.innerHTML, "window", "height=400, width=700")
+function tweetIt() {
+  window.open(
+    "https://twitter.com/intent/tweet?text=" +
+      quotes.innerHTML +
+      "---by " +
+      writer.innerHTML,
+    "window",
+    "height=400, width=700"
+  );
 }
